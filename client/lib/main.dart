ffb04bb82211ac16ceaed1cf6c739a5b30e0a2b3
saviya98 <savithdissanayake98@gmail.com>
2022-03-07 21:50:43 +0530

    Bottom Navigation Completed

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:client/screens/admin/flowerEntry.dart';
+import 'package:client/screens/home.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,21 +13,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flower App',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.lightGreen,
+        secondaryHeaderColor: Colors.white,
         scaffoldBackgroundColor: Color.fromARGB(255, 254, 255, 222),
       ),
-      home: FlowerEntry(),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const Home(),
+        '/adminAdd': (context) => const FlowerEntry(),
+      },
     );
   }
 }
---
+++ client/lib/screens/home.dart
@@ -0,0 +1,85 @@
+import 'package:client/screens/diary/diary.dart';
+import 'package:client/screens/forum/forum.dart';
+import 'package:client/screens/profile/profile.dart';
+import 'package:client/screens/search/search.dart';
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  List screens = [
+    Search(),
+    Profile(),
+    Diary(),
+    Forum(),
+  ];
+  int currentIndex = 0;
+  String _title = 'Flower App';
+
+  void onTap(int index) {
+    setState(() {
+      currentIndex = index;
+      switch (index) {
+        case 0:
+          {
+            _title = 'Home';
+          }
+          break;
+        case 1:
+          {
+            _title = 'Profile';
+          }
+          break;
+        case 2:
+          {
+            _title = 'Diary';
+          }
+          break;
+        case 3:
+          {
+            _title = 'Forum';
+          }
+          break;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_title),
+        actions: [
+          IconButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/adminAdd');
+              },
+              icon: Icon(Icons.admin_panel_settings_outlined))
+        ],
+      ),
+      body: screens[currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+          onTap: onTap,
+          currentIndex: currentIndex,
+          selectedItemColor: Colors.white,
+          unselectedItemColor: Colors.grey,
+          backgroundColor: Colors.lightGreen,
+          type: BottomNavigationBarType.fixed,
+          unselectedFontSize: 0,
+          selectedFontSize: 0,
+          elevation: 1,
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.settings), label: 'Settings'),
+            BottomNavigationBarItem(icon: Icon(Icons.book), label: 'Diary'),
+            BottomNavigationBarItem(icon: Icon(Icons.forum), label: 'Forum'),
+          ]),
+    );
+  }
+}
